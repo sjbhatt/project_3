e8bd56c28fd2d7aa0389477219f71526e64ddebf
pbagirthi <pbagirthi@elearn.ai>
2019-07-09 21:02:57 -0700

    clickig images

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ def fav():
     """Fruit and veggie finder page."""
     return render_template("favfinder.html")
 
+@app.route("/relatedFavfinder")
+def relatedFavfinder():
+    """Fruit and veggie finder page."""
+    return render_template("relatedfavfinder.html")
+
 @app.route("/fruit_choice/<choice>")
 def fruit_choice(choice):
 
@@ -27,7 +32,7 @@ def fruit_choice(choice):
 
     # calculate 48 k-nearest neighors based on user 'choice'
     X = np.array(selection_df['features'].values.tolist())
-    nbrs = NearestNeighbors(n_neighbors=48, algorithm='kd_tree').fit(X)
+    nbrs = NearestNeighbors(n_neighbors=18, algorithm='kd_tree').fit(X)
     distances, indices = nbrs.kneighbors([X[int(choice)]])
     idx = list(np.ndarray.flatten(indices))
     selection_df = selection_df.iloc[idx,:]
@@ -35,6 +40,17 @@ def fruit_choice(choice):
 
     # output to json
     output_df = selection_df[['fruit_number','fruit', 'filename', 'distance']]
+    fruit_data = output_df.to_dict('records')
+    return jsonify(fruit_data)
+
+@app.route("/allFruits")
+def allFruits():
+
+    # create dataframe where features are converted to a list
+    selection_df = pd.read_csv('static/fruits_sample.csv')
+    
+    # output to json
+    output_df = selection_df[['fruit_number','fruit', 'filename']]
     fruit_data = output_df.to_dict('records')
     return jsonify(fruit_data)
 
